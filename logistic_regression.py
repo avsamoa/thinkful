a66607f93ad9b7c4d39c023107e3676472f983b5
avsamoa <avaai@ecarruthers.com>
2015-04-08 22:44:15 +1300

    Update logistic_regression.py

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,7 +1,20 @@
 import pandas as pd
 import statsmodels.api as sm
+import math
 
-df=pd.read_csv('loansData_clean.csv')
+loansData=pd.read_csv('loansData_clean.csv')
+loansData['IR_TF']=loansData['Interest.Rate']<.12
+loansData['Intercept']
 
-df['Int.Rate.Less.12']=df['Interest.Rate'].map(lambda x: x<=.12)
+ind_vars=['FICO.Score','Amount.Requested','Intercept']
+logit=sm.Logit(loansData['IR_TF'],loansData[ind_vars])
+result=logit.fit()
 
+coeff=result.params
+print coeff
+
+def logistic_function(fico,loanamt,coeff):
+
+    z = coeff[0]*fico + coeff[1]*loanamt + coeff[2]
+
+    return 1/(1+math.exp(-1*z))
